1c4945afe3b7a7212c41f8b218453f8a37036ce3
agate-pris <agate-pris@users.noreply.github.com>
2021-12-01 03:23:40 +0900

    Update gitconfig.th

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 git config --global core.editor 'code -nw'
-git config --global difftool.code.cmd 'code -dnw "$LOCAL" "$REMOTE"'
+# git config --global credential.helper store
 git config --global diff.guitool 'code'
+git config --global difftool.code.cmd 'code -dnw "$LOCAL" "$REMOTE"'
+git config --global merge.guitool 'code'
 git config --global mergetool.code.cmd 'code -nw "$MERGED"'
 git config --global mergetool.code.trustExitCode false
-git config --global merge.guitool 'code'
-echo "*** Please tell me who you are.
-
-Run
-
-  git config --global user.email \"you@users.noreply.github.com\"
-  git config --global user.name \"Your Name\"
-
-to set your account's default identity.
-Omit --global to set the identity only in this repository."
+# git config --global user.email agate-pris@users.noreply.github.com
+# git config --global user.name agate-pris
+git config --global user.useConfigOnly true
+git config --global url.https://agate-pris@github.com/agate-pris/.insteadOf https://github.com/agate-pris/
