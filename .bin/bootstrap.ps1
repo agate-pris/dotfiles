3e720b4f9e7884423312d67c8d5098480d37db07
agate-pris <agate-pris@users.noreply.github.com>
2021-12-31 17:26:43 +0900

    Update bootstrap.ps1

--- .bin/bootstrap.ps1
+++ .bin/bootstrap.ps1
@@ -1,3 +1,6 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope Process
 Get-ExecutionPolicy -List
+iwr -useb get.scoop.sh | iex
+scoop install sudo
+sudo scoop install 7zip git openssh --global
