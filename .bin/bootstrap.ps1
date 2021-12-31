d9b91b4ea305de246d4180027ff8c9052e3c2c69
agate-pris <agate-pris@users.noreply.github.com>
2021-12-31 17:30:55 +0900

    Update bootstrap.ps1

--- .bin/bootstrap.ps1
+++ .bin/bootstrap.ps1
@@ -1,6 +1,11 @@
+echo bar
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope Process
 Get-ExecutionPolicy -List
+echo baz
 iwr -useb get.scoop.sh | iex
+echo qux
 scoop install sudo
+echo quux
 sudo scoop install 7zip git openssh --global
+echo quuux
